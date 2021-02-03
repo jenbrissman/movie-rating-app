3d18fdc4fc9cea86d271e34074da204b46f6caf5
Ilana Rose Mercer <ilana.rose.mercer@gmail.com>
2021-02-03 20:46:14 +0000

    Creates User Class

--- model.py
+++ model.py
@@ -4,8 +4,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    """A user."""
 
-# Replace this with your code!
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True,)
+    email = db.Column(db.String, unique=True,)
+    password = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
 
 
 def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
